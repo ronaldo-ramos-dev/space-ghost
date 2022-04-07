92c480a9b15b9d487df3d0ff6e2e40fd342f24e8
Ronaldo Ramos <rramos@crawfordtech.com>
2022-04-06 23:16:09 -0400

    start here

---
+++ main.py
@@ -0,0 +1,79 @@
+from pickle import FALSE
+import pygame
+from player import Player
+from enemy import Enemy
+from settings import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+def run():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    enemies = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+    
+    RUNNING = True
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    ADDENEMY = pygame.USEREVENT + 1
+    pygame.time.set_timer(ADDENEMY, 250)
+
+    player = Player()
+
+    all_sprites.add(player)   
+
+    while RUNNING:
+
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+
+                if event.key == K_ESCAPE:
+                    RUNNING = False
+
+            elif event.type == pygame.QUIT:
+                RUNNING = False
+
+            elif event.type == ADDENEMY:
+                new_enemy = Enemy()
+                enemies.add(new_enemy)
+                all_sprites.add(new_enemy)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        player.update(pressed_keys)
+
+        enemies.update()
+
+        screen.fill((0,0,0))
+
+        for entity in all_sprites:
+            screen.blit(entity.surf, entity.rect)
+
+        if pygame.sprite.spritecollideany(player, enemies):
+            player.kill()
+            RUNNING = False
+
+        pygame.display.flip()
+
+        clock.tick(250)
+
+
+    pygame.quit()
+
+
+
+
+if __name__ == '__main__':
+    run()    
---
+++ player.py
@@ -0,0 +1,42 @@
+import pygame
+from settings import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_SPEED
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75,25))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
